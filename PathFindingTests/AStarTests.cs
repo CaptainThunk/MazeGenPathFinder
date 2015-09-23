d255c6126310a4dbc9faeb200d8b8ecc2cff00c5
Dean Becker <dean.becker@nexusinds.com>
2015-09-23 16:36:01 +0100

    Added A* unit test shell

---
+++ PathFindingTests/AStarTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PathFinding.Solvers;
+using MazeGeneration;
+
+namespace PathFindingTests
+{
+    [TestClass]
+    public class AStarTests
+    {
+        private AStar solver;
+        private Maze maze;
+        private int mazeWidth = 5, mazeHeight = 5;
+
+        [TestInitialize]
+        public void Init()
+        {
+            maze = new Maze(mazeWidth, mazeHeight);
+            solver = new AStar(maze);
+        }
+
+        [TestMethod]
+        public void MyTestMethod()
+        {
+            Assert.IsInstanceOfType(solver, typeof(AStar));
+        }
+    }
+}
